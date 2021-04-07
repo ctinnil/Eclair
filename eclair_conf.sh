9fa27c8247ae1d9220c66e67d2cf23907574e216
ctinnil <69745175+ctinnil@users.noreply.github.com>
2021-04-07 14:22:56 +0300

    Add files via upload

--- eclair_conf.sh
+++ eclair_conf.sh
@@ -100,7 +100,7 @@ echo "Done."
 # install xcode command line tool
 xcode-select --install
 xcode-select -p
-echo "Xcode command line tools installed ..."
+echo "Xcode command line tools installed ... "
 
 sleep 60
 
@@ -128,50 +128,102 @@ echo "Set Eclair workstation wallpaper"
 cp ./res/lockscreen.jpeg /tmp/lockscreen.jpeg
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/tmp/lockscreen.jpeg"'
 
-#############
-# Utilities #
-#############
-brew install mas 
-brew install tree 
-brew install p7zip 
-brew install cmake
-brew install midnight-commander 
+PACKAGES=(
+    ack #a beyond grep-like source code search tool
+    apache-spark #a engine for large-scale data processing
+    aria2 #a flexible download utility software with multi-protocol and multi-source support
+    caffe #a fast open framework for deep learning
+    cmake #a family of tools to build, test and package software
+    ffmpeg #a large suite of libraries and programs for handling video, audio, and other multimedia files and streams
+    fzf #a command-line fuzzy finder written in Go
+    gettext #a multilingual programs writing utility for Unix-like computer operating systems
+    gifsicle #a command-line tool for creating, editing, and getting information about GIF images and animations
+    git #a software for tracking changes in any set of files, usually used for coordinating work among programmers/ should be installed
+    graphviz #a package of open-source tools for drawing graphs
+    hadoop #a framework for distributed processing of large data sets
+    hub #an extension to command-line git
+    icdiff #an improved colored diff
+    imagemagick #a cross-platform software suite for displaying, creating, converting, modifying, and editing raster images
+    jq #a lightweight and flexible command-line JSON processor
+    kafka #a framework implementation of a software bus using stream-processing
+    libjpeg #a free library with functions for handling the JPEG image data format
+    libmemcached #an open source C/C++ client library and tools for the memcached server 
+    llvm #a next-gen compiler infrastructure
+    lynx #a command-line web browsers
+    macvim #a GUI for vim, made for macOS
+    markdown #a lightweight markup language for creating formatted text using a plain-text editor
+    mas #Mac App Store command-line interface
+    memcached #a distributed memory object caching system
+    mercurial #a distributed revision control tool for software developers
+    midnight-commander #a visual file manager
+    mosh #a replacement for interactive SSH terminals
+    npm #a package manager for the JavaScript 
+    nvm #Manage multiple Node.js versions
+    onednn #Basic building blocks for deep learning applications
+    open-mpi #a high performance message passing library
+    pkg-config #a computer program that defines and supports
+    postgresql #a free and open-source relational database management  
+    python3 #an interpreted, high-level and general-purpose programming language
+    pypy #an alternative implementation of the Python programming language to CPython
+    p7zip #7-Zip (high compression file archiver) implementation
+    r #a software environment for statistical computing
+    rabbitmq #an open-source message-broker software 
+    scala #JVM-based programming language
+    termius #a CLI tool for termius.com
+    the_silver_searcher #a code searching tool similar to ack, with a focus on speed
+    tmux #an open-source terminal multiplexer for Unix-like operating systems
+    trash-cli #a command-line interface to the freedesktop.org trashcan
+    tree #a tiny, cross-platform command-line program used to recursively list or display the content of a directory in a tree-like format
+    vim #a clone of vi text editor
+    wget #a computer program that retrieves content from web
+    wiki #a command-line Wikipedia search tool
+)
 
-brew install --cask keka
-brew install --cask firefox
-brew install --cask google-chrome
+CASKS=(
+    anaconda #Continuum Analytics Anaconda
+    db-browser-for-sqlite #DB Browser for SQLite
+    deluge #Deluge - BitTorrent client
+    docker #Docker Desktop
+    dotnet-sdk #.NET SDK
+    drawio #draw.io Desktop
+    eclipse-java #Eclipse IDE for Java Developers
+    enpass #Enpass
+    firefox #Mozilla Firefox
+    github #GitHub Desktop
+    google-chrome #Google Chrome
+    gpg-suite-pinentry #GPG Suite Pinentry
+    keka #Keka - File archiver
+    knime #KNIME Analytics Platform
+    mactex #MacTeX - Full TeX Live distribution with GUI applications
+    microsoft-remote-desktop #Microsoft Remote Desktop
+    miniconda #Continuum Analytics Miniconda
+    merlin-project #Merlin Project
+    netbeans #NetBeans IDE 
+    oracle-jdk #Oracle Java Standard Edition Development Kit
+    oracle-jdk-javadoc #Oracle Java Standard Edition Development Kit Documentation
+    orange #Orange - Component-based data mining software
+    projectlibre #ProjectLibre
+    pycharm-ce #Jetbrains PyCharm Community Edition
+    rapidminer-studio #RapidMiner Studio
+    spectacle #Spectacle - Move and resize windows with ease
+    sublime-text #Sublime Text
+    termius #Termius
+    vagrant #Vagrant
+    virtualbox #Oracle VirtualBox
+    visual-studio #Microsoft Visual Studio
+    visual-studio-code #Microsoft Visual Studio Code
+    vlc #VLC media player
+    weka #Weka
+    xmind #XMind - Mind mapping and brainstorming tool
+)
 
-#ddgo
-ddgo=$(mas search duckduckgo | head -n1 | tr -s " " | cut -f2 -d" " )
-mas install $ddgo  # must be sign in with Apple ID
+echo ""
+echo "Installing packages ... "
+brew install ${PACKAGES[@]}
 
-#enpass
-enpass=$(mas search enpass | head -n1 | tr -s " " | cut -f2 -d" " )
-mas install $enpass
-
-############
-# Planning #
-############
-brew install --cask xmind 
-brew install --cask drawio
-
-brew install --cask projectlibre
-brew install --cask merlin-project
-
-####################
-# Data acquisition #
-####################
-brew install curl
-brew install wget 
-
-brew install --cask deluge
-
-#################
-# Visualization #
-#################
-brew install graphviz
-
-brew install --cask db-browser-for-sqlite
+echo ""
+echo "Installing cask apps ... "
+brew cask install ${CASKS[@]}
 
 echo ""
 echo "Elastic Stack setup ... "
@@ -183,34 +235,17 @@ brew install elastic/tap/kibana-full
 brew services start elastic/tap/kibana-full
 #To access Kibana go to :::> http://localhost:5601
 
-###############
-# Development #
-###############
-brew install --cask github
+echo ""
+echo "Installing Xcode and Swift Playgrounds ... "
+#xcode
+xcode=$(mas search xcode | head -n1 | tr -s " " | cut -f2 -d" " )
+mas install $xcode
+#swift
+swift=$(mas search swift | head -n1 | tr -s " " | cut -f2 -d" " )
+mas install $swift
 
-brew install caffe
-brew install scala
-brew install hadoop
-brew install onednn
-brew install python3 
-brew install open-mpi
-brew install apache-spark
-brew install --cask swift
-#brew install --cask dotnet 
-brew install --cask anaconda
-brew install --cask dotnet-sdk
-brew install --cask pycharm-ce 
-brew install --cask sublime-text 
-brew install --cask netbeans
-brew install --cask visual-studio-code
-brew install --cask eclipse-jee
-
-brew install java go r
-brew install --cask oracle-jdk
-brew install --cask oracle-jdk-javadoc
-brew install --cask visual-studio
-
-brew install nvm
+echo ""
+echo "npm package manager installing and configuring ... "
 mkdir ~/.nvm
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
@@ -221,65 +256,62 @@ nvm install $nvm_ver
 npm install -g npm
 npm i -g dependency-cruiser
 
-
+echo ""
+echo "Installing Python packages ... "
 python3 -m pip install --upgrade setuptools
 python3 -m pip install --upgrade pip
-python3 -m pip install -U numpy scipy ipython jupyter pandas sympy nose
-python3 -m pip install -U matplotlib seaborn plotly
-python3 -m pip install -U times joblib 
-#python3 -m pip install -U OpenNN 
-python3 -m pip install -U h2o
-python3 -m pip install -U scikit-learn
-python3 -m pip install -U tensorflow
-#python3 -m pip install -U torch torchvision torchaudio
-python3 -m pip install -U cntk cntk-gpu
 
-brew install llvm
-echo 'export PATH="/usr/local/opt/llvm/bin:$PATH"' >> ~/.bash_profile
+PYTHON_PACKAGES=(
+    cntk
+    cortex
+    h2o
+    ipython
+    joblib
+    jupyter
+    jupyterlab
+    matplotlib
+    nose
+    numpy 
+    opencv
+    pandas
+    plaidml-keras
+    plotly
+    pyopencl
+    scikit-image
+    scikit-learn
+    scipy 
+    seaborn
+    shogun
+    sympy
+    tensorflow
+    times
+    torch 
+    torchaudio
+    torchvision
+    tqdm
+    virtualenv
+    virtualenvwrapper
+)
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-
-conda activate ai
-conda install pytorch torchvision torchaudio cudatoolkit=11.0 -c pytorch
-conda deactivate ai
-conda install -c conda-forge shogun
-conda install -c conda-forge jupyterlab
-
-#xcode
-xcode=$(mas search xcode | head -n1 | tr -s " " | cut -f2 -d" " )
-mas install $xcode
-
+sudo python3 -m pip install -U ${PYTHON_PACKAGES[@]}
 git clone git://github.com/pybrain/pybrain.git
-python3 -m pip install -U -e pybrain
+sudo python3 -m pip install -U -e pybrain
 
-############
-# Modeling #
-############
-brew install --cask weka
-brew install --cask knime
-brew install --cask orange
-brew install --cask rapidminer-studio
+echo ""
+echo "Installing Ruby gems ... "
+RUBY_GEMS=(
+    bundler
+    filewatcher
+    cocoapods
+)
+sudo gem install ${RUBY_GEMS[@]}
 
-##################
-# Virtualization #
-##################
-brew install --cask docker
-brew install --cask virtualbox 
-
-##########
-# Remote #
-##########
-brew install mosh 
-brew install --cask termius
-
-#rdp
-rdp=$(mas search microsoft-remote | head -n1 | tr -s " " | cut -f2 -d" " )
-mas install $rdp
-
-docker pull ibmcom/fhe-toolkit-ubuntu
-docker pull nvidia/cuda
+echo ""
+echo "Initialize Docker containers ... "
+git clone https://github.com/IBM/fhe-toolkit-macos.git
+cd fhe-toolkit-macos/dependencies/
+./setup.sh
+cd ../..
 
 #check installs
 brew doctor 
@@ -289,9 +321,5 @@ brew cleanup
 
 # enable terminal autocomplet 
 set show-all-if-ambiguous on
-
-#add bookmarks 
-sudo chmod +ux addbookmarks.sh
-./addbookmarks.sh "Eclair Workstation" "HomePage https://ctinnil.github.io/Eclair/, Project https://github.com/ctinnil/Eclair, Homebrew https://formulae.brew.sh"
 
 echo "Setup complete !!!"
